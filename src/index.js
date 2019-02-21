ffb93700b305def407bc570bcfff57a5926866ac
Kevin van der Poll <vdpkevin@gmail.com>
2019-02-21 17:36:50 -0500

    Rename files

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom";
 import { createBrowserHistory } from "history";
 import { Router, Route, Switch } from "react-router-dom";
 
-import indexRoutes from "src/routes/index.jsx";
+import indexRoutes from "routes/index.js";
 
 import "assets/scss/material-kit-react.css?v=1.3.0";
 
